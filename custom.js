9b78b55061c1560d41a9e4e7d6e5cae7e2c41a94
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-10-01 02:53:35 +0500

    create variable and bring Id from DOM and create arrow function

---
+++ custom.js
@@ -0,0 +1,6 @@
+const password = document.getElementById('password')
+const background = document.getElementById('background')
+
+password.addEventListener('input', (e) => {
+    const input = e.target.value
+})
